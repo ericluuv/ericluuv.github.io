acede5934acabad987e131b8f5dd17ad12325644
Eric Luu <41553418+ericluuv@users.noreply.github.com>
2020-09-21 17:42:28 -0700

    Create indexScript.js

---
+++ indexScript.js
@@ -0,0 +1,15 @@
+"use strict"
+
+function pathClick()
+{
+    var x = document.getElementById("path-troubleshoot");
+    if (x.style.display == "none")
+    {
+        x.style.display = "block";
+    }
+    else 
+    { 
+        x.style.display = "none"; 
+    }
+}
+
